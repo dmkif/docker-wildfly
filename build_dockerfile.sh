eaf89731d2c06c99b0ade6601ca6b5bf15bad03d
dmkif <daniel.mulzer@fau.de>
2018-05-03 11:59:45 +0200

    source out processes to scriptfiles - fix

--- build_dockerfile.sh
+++ build_dockerfile.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-if [ $ARCH = "amd64" ]
-then
-    sed s/"@@ARCH_2@@"/"x84_64"/g Dockerfile.$ARCH
-elif [ $ARCH = "arm64v8"]
-then
-    sed s/"@@ARCH_2@@"/"aarch64"/g Dockerfile.$ARCH
-else
-    sed s/"@@ARCH_2@@"/"@@ARCH@@"/g Dockerfile.$ARCH
+if [ "$ARCH" = "amd64" ]
+  then
+    sed s/"@@ARCH_2@@"/"x84_64"/g Dockerfile > Dockerfile.$ARCH
+  elif [ "$ARCH" = "arm64v8" ]
+  then
+    sed s/"@@ARCH_2@@"/"aarch64"/g Dockerfile > Dockerfile.$ARCH
+  else
+    sed s/"@@ARCH_2@@"/"@@ARCH@@"/g Dockerfile > Dockerfile.$ARCH
 fi 
 sed -i s/"@@ARCH@@"/"$ARCH"/g Dockerfile.$ARCH
--- deploy_container.sh
+++ deploy_container.sh
@@ -3,10 +3,11 @@
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORT"
 
 #if branch is master tag image as latest and push it
-if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = "master" ]; then
-   docker tag  $REPO-$ARCH:$TAG $REPO-$ARCH:$TRAVIS_BUILD_NUMBER
-   docker push $REPO-$ARCH:$TRAVIS_BUILD_NUMBER
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]
+ then
+   docker tag  "$REPO-$ARCH:$TAG" "$REPO-$ARCH:$TRAVIS_BUILD_NUMBER"
+   docker push "$REPO-$ARCH:$TRAVIS_BUILD_NUMBER"
 fi 
-docker push $REPO-$ARCH:$TAG
+docker push "$REPO-$ARCH:$TAG"
 
 docker logout
